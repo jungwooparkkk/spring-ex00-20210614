249e499f7ee281c0645e3504fb1b78ed34799500
Jungwooparkkk <pjw7935@naver.com>
2021-06-17 16:26:01 +0900

    spring

---
+++ src/main/java/org/zerock/domain/SampleDTO.java
@@ -0,0 +1,11 @@
+package org.zerock.domain;
+
+import lombok.Data;
+
+@Data
+public class SampleDTO {
+	// 129쪽
+	private String name;
+	private int age;
+	
+}
---
+++ src/main/java/org/zerock/domain/TodoDTO.java
@@ -0,0 +1,13 @@
+package org.zerock.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class TodoDTO {
+	//136쪽
+	private String title;
+	private Date dueDate;
+	
+}
